aed14cc30ede18d17b1f06d0b07a8a8c02fbbb8b
SneyderHOL <1507@holbertonschool.com>
2021-12-09 00:37:18 -0500

    [UPDATE] fix non used variable

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -2,9 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  const a = (a, b) => a || b;
+
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
+    a(task, task2);
   }
 
   return [task, task2];
