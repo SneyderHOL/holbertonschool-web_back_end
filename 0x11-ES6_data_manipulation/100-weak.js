64031faa911840dafd1fd1c6b65cbc4e8f43ba0f
Eduard Sneyder Amador Valbuena <1507@holbertonschool.com>
2021-12-22 22:06:12 -0500

    [UPDATE]: change set value before condition

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -2,10 +2,10 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1 + weakMap.get(endpoint));
     if (weakMap.get(endpoint) >= 5) {
       throw Error('Endpoint load is high');
     }
-    weakMap.set(endpoint, 1 + weakMap.get(endpoint));
   } else {
     weakMap.set(endpoint, 1);
   }
