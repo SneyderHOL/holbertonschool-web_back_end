2dc430519307e5aa481eb2a97edae671e65f0655
Eduard Sneyder Amador Valbuena <1507@holbertonschool.com>
2022-01-03 22:05:30 -0500

    [UPDATE]: fix input

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
-const encoding = 'utf-8';
 console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  process.stdout.write(`Your name is: ${input}`);
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
