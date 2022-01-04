7751d1f218a2caad67317d9b8e2a273f49d658ef
Eduard Sneyder Amador Valbuena <1507@holbertonschool.com>
2022-01-03 22:55:03 -0500

    [UPDATE]: fix routes

--- 0x12-Node_JS_basic/full_server/server.js
+++ 0x12-Node_JS_basic/full_server/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import router from './routes/index';
+import routes from './routes/index';
 
 const app = express();
 const port = 1245;
 
-app.use('/', router);
+routes(app);
 
 app.listen(port, () => {
   //   console.log(`Example app listening at http://localhost:${port}`);
