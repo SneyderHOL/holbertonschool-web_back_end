89253f28d813cf820c400cb6d7c59e13a9fffff2
Eduard Sneyder Amador Valbuena <1507@holbertonschool.com>
2022-01-09 22:27:50 -0500

    [UPDATE]: add json method

--- 0x13-unittests_in_js/10-api/api.js
+++ 0x13-unittests_in_js/10-api/api.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.end('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.send(`Payment methods for cart ${req.params.id}`);
+  res.end(`Payment methods for cart ${req.params.id}`);
 });
 
 app.get('/available_payments', (req, res) => {
